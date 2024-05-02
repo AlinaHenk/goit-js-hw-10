b9767a7b940a94497ae524e27179c19f967f03a0
AlinaHenk <160937471+AlinaHenk@users.noreply.github.com>
2024-05-02 16:25:40 +0300

    without snackbar

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,74 +1,81 @@
-
-
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 const timerForm = document.querySelector("#datetime-picker");
-const timerDays = document.querySelector("#data-days");
-const timerHours = document.querySelector("#data-hours");
-const timerMinutes = document.querySelector("#minutes-days");
-const timerSeconds = document.querySelector("#data-seconds");
+const timerDays= document.querySelector('span[data-days]');
+const timerHours = document.querySelector('span[data-hours]');
+const timerMinutes = document.querySelector('span[data-minutes]');
+const timerSeconds = document.querySelector('span[data-seconds]');
 const btn = document.querySelector('button');
-btn.addEventListener("click", setTimer);
-
 let userSelectedDate;
+let timerId;
 
-let date = new Date();
+const addLeadingZero = value => 
+ String(value).padStart(2, '0');
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+  }
+
 const options = {
   enableTime: true,
   time_24hr: true,
-  defaultDate: date,
+  defaultDate: new Date(),
   minuteIncrement: 1,
-    onClose(selectedDates) {
-        if (selectedDates[0] > date) {
+  onClose(selectedDates) {
+          if (selectedDates[0] > options.defaultDate) {
           userSelectedDate = selectedDates[0];
-          console.log(userSelectedDate);
-          btn.removeAttribute("disabled");;
+          btn.removeAttribute("disabled");
         }
         else {
           iziToast.warning({
           color:'#EF4040', position: 'topRight', messageColor: '#FAFAFB', iconColor: '#FFFFFF',
           message: 'Please choose a date in the future',
         });  
-          //  window.alert("Please choose a date in the future");
-          // localStorage.setItem("selectedDate",selectedDates[0]); 
           btn.setAttribute('disabled', 'true');
         }
   },
 };
 
+
 const fp = flatpickr(timerForm, options);
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
+function countdownTimer() {
+  const ms = userSelectedDate.getTime() - Date.now();
+  if (ms <= 0) {
+    clearInterval(timerId);
+timerForm.removeAttribute("disabled");
+    return;
+  }
+  const { days, hours, minutes, seconds } = convertMs(ms);
+  timerDays.textContent = days;
+  timerHours.textContent = hours;
+  timerMinutes.textContent = minutes;
+  timerSeconds.textContent = seconds;
 }
 
+btn.addEventListener("click", setTimer);
+ 
 function setTimer(event) {
-  let timeRemain = userSelectedDate.getTime() - Date.now();
-  console.log(userSelectedDate, timeRemain);
-  const convertedTime = convertMs(timeRemain);
- // const str = convertedTime.padStart(2,'0');
-  console.log(convertedTime);
-
-}
-function timer() {
-     
-   }
+  btn.setAttribute('disabled', 'true');
+  timerForm.setAttribute('disabled', 'true');
+ if (userSelectedDate.getTime() - Date.now() <= 0) {
+ clearInterval(timerId);  
+  }
+else {
+  timerId = setInterval(countdownTimer, 1000);
+  } 
+  };
 
 
